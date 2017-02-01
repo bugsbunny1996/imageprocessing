62be829871e3c50033b017e80fc48bacf058fb02
bugsbunny1996 <sidda2158@gmail.com>
2017-02-01 21:30:09 +0530

    Added test images set 2 /testimages/ and FAST
    Fast feature extraction has been added and older picture classification
    has been changed. tic program toc = 11.22 seconds

--- templatematch.m
+++ templatematch.m
@@ -1,16 +1,17 @@
-info = mha_read_header('C:\Users\rabbit\Documents\Github\imageprocessing\1_T1.mha');
+info = mha_read_header('C:\Users\rabbit\Documents\Github\imageprocessing\volume_1.mha');
 image = mha_read_volume(info);
 train_abnormal_image =gpuArray(image(20:150,20:200,1));
 train_normal_image =gpuArray(image(20:150,20:200,1));
-image_abnormality_index=[0];
-image_normal_index=[0];
-array=[0];
+arrayab=gpuArray(zeros(20,2,1));
+arrayno =gpuArray(zeros(20,2,1));
+temp= gpuArray(zeros(50,2));
 count_1 = 0;
 count_2 = 0;
 template1 =rgb2gray(imread('C:\Users\rabbit\Pictures\template.png'));
 template2 = rgb2gray(imread('C:\Users\rabbit\Pictures\template2.png'));
 template1 = gpuArray(template1);
 template2 = gpuArray(template2);
+
 for i = 61:120
 img = (image(20:150,20 :200,i)); %ith image for searching
 R2D = mat2gray(img);
@@ -29,43 +30,66 @@ c_cpu2 = gather(c2);
 [ypeak2, xpeak2] = find(c_cpu2==max(c_cpu2(:)));
 temp2 = c_cpu2(ypeak2, xpeak2);
 
-
     if ((temp2)>(0.40)|| (temp1>0.44))
         count_1= count_1 +1;
-        train_abnormal_image(:,:,count_1) = img;
+%         train_abnormal_image(:,:,count_1) = img; 
+        points = detectFASTFeatures(img); 
+        points = points.selectStrongest(20);
+        temp = points.Location;
+        arrayab(:,:,count_1) = temp;
+        
     else
         count_2 = count_2 + 1;
-        train_normal_image(:,:,count_2)= img;
+%         train_normal_image(:,:,count_2)= img;
+        points = detectFASTFeatures(img); 
+        points = points.selectStrongest(20);
+        temp = points.Location;
+        arrayno(:,:,count_2) = temp;
     end
-     
+    
+end
+
+
+    group = ones(count_1,1);
+    for i=1:count_2
+        group = cat(1,group,2);   
+    end
+    
   
-end
-size_1 = size(train_abnormal_image);
-size_1 = size_1(3);
-size_2 = size(train_normal_image);
-size_2 = size_2(3);
-group = ones(size_1,1);
-for i=1:size_2
-    group = cat(1,group,2);   
-end
+     for i=count_1+1: count_1+ count_2
+%          train_abnormal_image(:,:,i) = train_normal_image(:,:,i-size_1);
+           arrayab(:,:,i) = arrayno(:,:,i-count_1);
+     end
+    for i=1:count_1+count_2
+%         images{i} = gather(train_abnormal_image(:,:,i));
+        images{i} = gather(arrayab(:,:,i));
+    end
+    
+    trainData = zeros(count_1 + count_2, 2 * 20 );
+    
+    for ii=1:count_1 + count_2
+          images{ii} = reshape(images{ii}', 1, size(images{ii},1)*size(images{ii},2));
+          trainData(ii,:) = images{ii};
+         
+    end
 
-for i=size_1+1: size_1+size_2
-    train_abnormal_image(:,:,i) = train_normal_image(:,:,i-size_1);
-end
-for i=1:size_1+size_2
-    images{i} = gather(train_abnormal_image(:,:,i));
-end
-
-trainData = zeros(size_1 + size_2,131*181);
-for ii=1:size_1 + size_2
-    images{ii} = reshape(images{ii}', 1, size(images{ii},1)*size(images{ii},2));
-    trainData(ii,:) = images{ii};
-end
 SVMStruct = svmtrain (trainData,group);
-inputImg = rgb2gray(imread('C:\Users\rabbit\Pictures\testimage2.png'));
+
+inputImg = (imread('C:\Users\rabbit\Documents\Github\imageprocessing\testimage2.png'));
+if size(inputImg,3)== 3
+    inputImg = rgb2gray(inputImg);
+end
+
 inputImg = im2double(inputImg);
-inputImg = inputImg(40:230,20:280);
-inputImg = imresize(inputImg, [131 181]);
-inputImg = reshape (inputImg', 1, size(inputImg,1)*size(inputImg,2));
-result = svmclassify(SVMStruct, inputImg);
-disp(result);
+inputImgpoints = detectFASTFeatures(inputImg);
+inputImgpoints = inputImgpoints.selectStrongest(20);
+inputImgpoints = inputImgpoints.Location;
+imshow(inputImg); hold on;
+points = detectFASTFeatures(inputImg);
+if size(points,1) < 20 
+
+end
+plot(points.selectStrongest(20));
+inputImg = reshape (inputImgpoints', 1, size(inputImgpoints,1)*size(inputImgpoints,2));
+result = svmclassify(SVMStruct, inputImg)
+
